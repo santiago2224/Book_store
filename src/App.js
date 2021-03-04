48e23a883140bc5cb7c3f4e9971923c6eeeeef22
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-03-04 12:01:35 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
+const [count, setCount] = useState(0)
+  const initial = 0 
+  function minusCount() {
+    setCount(count -1)
+  }
+  function addCount() {
+    setCount(count +1)
+  }
+  function resetCount () {
+    setCount(initial)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Book List</h1>
+    <button onClick={minusCount}>Search Author</button>
+    <button onClick={addCount}>Search Title</button>
+    <button onClick={resetCount}>Search ISBN</button>
+    </>
   );
 }
 
